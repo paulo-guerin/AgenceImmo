bc63ddd2fb88686492de6b6f6995f3e18806a8bd
ezekiel <florian.vidal@lapiscine.pro>
2020-03-09 17:06:25 +0100

    commt de ses morts

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Reponse;
+
+class HomeController{
+
+    public function home()
+    {
+        return new Response();
+
+    }
+}
